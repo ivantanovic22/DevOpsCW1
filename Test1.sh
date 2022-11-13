27be828148f984dc11bc7aaddeffd2ed8f61cb42
Ubuntu <ubuntu@ip-172-31-28-171.ec2.internal>
2022-11-13 17:53:23 +0000

    Added Tests

--- Test1.sh
+++ Test1.sh
@@ -1,12 +1,34 @@
-source Test1.sh
-
-$output
-
-expected =$(" 0.74 Pounds ")
-
-if [[ "$result" == "${expected}" ]]; then 
-		echo "Thanks for using the converter!"
+#!/bin/bash
+amount="1 dollars"
+var=[ java CurrencyConverter $amount ]
+if [ "$var" == 0.74 ]
+then
+        echo "Thanks"
 else
-		echo "Please input a value"
-		exit 1
+        echo "failed"
 fi
+if [ ! -n "$var" ]
+then
+      echo "A Value must be inputted"
+else
+  echo "Test Passed"
+fi
+if [ "$amount" == "1 dollars" ] || [ "$amount" == "1 pounds" ] || [ "$amount" == "1 euros" ]
+then 
+	echo "Test Passed"
+else 
+	echo "Input must be in correct format e.g 50 dollars"
+fi
+if [ "$amount" == "1 Dollars" ] || [ "$amount" == "1 Pounds" ] || [ "$amount" == "1 Euros" ]
+then 
+	echo "Test Passed"
+else
+	echo "Test Failed"
+fi
+if [ "$amount" == "1 DOLLARS" ] || [ "$amount" == "1 POUNDS" ] || [ "$amount" == "1 EUROS" ]
+then
+        echo "Test Passed"
+else
+        echo "Test Failed"
+fi
+
