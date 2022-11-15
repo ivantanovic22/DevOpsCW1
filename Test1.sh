fd1ffd9f1cdb6ae101bc9cf9b78cf3298a0cb4a9
Ubuntu <ubuntu@ip-172-31-28-171.ec2.internal>
2022-11-15 20:19:30 +0000

    Demo Test Update

--- Test1.sh
+++ Test1.sh
@@ -4,7 +4,7 @@ correctP="1.0 Pounds = 1.36 Dollars 1.0 Pound = 1.19 Euros Thank you for using t
 correctD="1.0 Dollars = 0.74 Pounds 1.0 Dollars = 0.88 Euros Thank you for using the converter."
 correctE="1.0 Euros = 1.31 Dollars 1.0 Euros = 0.84 Pounds Thank you for using the converter."
 var=$(java CurrencyConverter $amount)
-
+	echo "Welcome"
 if [ ! -n "$var" ]
 then
        echo "A Value must be inputted"
